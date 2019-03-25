ca7763778c71dc614ba411b9187eb1771295d48c
nunomiguel22 <37627936+nunomiguel22@users.noreply.github.com>
2019-03-25 17:11:43 +0000

    file analysis test

--- Project 1/forensic.c
+++ Project 1/forensic.c
@@ -11,78 +11,107 @@ void signal_handler(int signo) //handler do sinal
 
 } 
 
+int analyze_file (char *filepath) {
+	/*
+		Comando stat(man 2 stat), da um struct com file_size(st_size), file_access(st_mode), 
+		file_modification_date(st_mtime), file_created(st_ctime, last status change?)	
+		Comando file da file_type(?)
+		
+		TO-DO:
+			-chamar comando file, para obter file type
+			-Formatar informaçao st_mode, (owner, group, others?)
+			-Calcular md5, sha1, sha256 quando necessario
+			-printf da informaçao ou gravar em ficheiro quando o_commmand diferente de null
+	*/
+	
+	struct stat statdata;
+	struct tm ct, mt;
+	char *creation_time = (char*) malloc (19);
+	char *mod_time = (char*) malloc (19);
+	
+	if (stat(filepath, &statdata) < 0){
+		perror("Error");
+		exit(1);	
+	}
+	
+	localtime_r(&statdata.st_ctime, &ct);
+	localtime_r(&statdata.st_mtime, &mt);
+	sprintf(creation_time, "%d-%d-%dT%d:%d:%d", 1900 + ct.tm_year, ct.tm_mon, ct.tm_mday, ct.tm_hour, ct.tm_min, ct.tm_sec);
+	sprintf(mod_time, "%d-%d-%dT%d:%d:%d", 1900 + mt.tm_year, mt.tm_mon, mt.tm_mday, mt.tm_hour, mt.tm_min, mt.tm_sec);
+	
+	//DEBUG
+	printf("file_name,file_type,%ld,file access (parse st_mode),%s,%s\n", statdata.st_size, creation_time,mod_time);
+	
+	free(creation_time);
+	free(mod_time);
+	
+	return 0;
+	
+	
+}
+
 int main (int argc, char *argv[], char *envp[]){
 
 	struct sigaction action;
-    	action.sa_handler = signal_handler;
-    	sigemptyset(&action.sa_mask);
+    action.sa_handler = signal_handler;
+    sigemptyset(&action.sa_mask);
 
-    	if (sigaction(SIGINT,&action,NULL) < 0) //tratamento de sinal associado ao ctrl+c
-    	{
-      	  fprintf(stderr,"Unable to install SIGTERM handler\n");
-      	  exit(1);
-    	}
+    if (sigaction(SIGINT,&action,NULL) < 0) //tratamento de sinal associado ao ctrl+c
+    {
+		fprintf(stderr,"Unable to install SIGTERM handler\n");
+		exit(1);
+    }
 	
-	//File or Dir path
-	char* filepath;
-	//-r flag, analyse all filesand subdirectories
-	bool r_command = false;
-	//-h flag, string with algorithms, null if not used
-	char *h_command = NULL;
-	//-v flag, generate log file, log file name in enviromnent variable LOGFILENAME
-	bool v_command = false;
-	//-o flag, save data in CSV file, string with filename, null if not used
-	char* o_command = NULL;
-	
-	/* Needs at least one argument */
-	if (argc < 2){
+	char* filepath;			//File or Dir path
+	bool r_command = false;	//-r flag, analyse all files and subdirectories
+	char *h_command = NULL;	//-h flag, string with algorithms, null if not used
+	bool v_command = false;	//-v flag, generate log file, log file name in enviromnent variable LOGFILENAME
+	char* o_command = NULL;	//-o flag, save data in CSV file, string with filename, null if not used
+		
+	if (argc < 2){	/* Needs at least one argument */
 		printf("forensic [-r] [-h [md5[,sha1[,sha256]]] [-o <outfile>] [-v] <file|dir>\n");
-		return -1;
+		exit(1);
 	}
 	
-	/* Parse arguments */
-	for (int i = 1; i < argc - 1; ++i){
+	for (int i = 1; i < argc - 1; ++i){	/* Parse arguments */
+		
 		if (!strcmp(argv[i], "-r")){
 			r_command = true;
+			continue;
 		}
+		
 		// TO-DO: Verify if correct argument(md5, sha1, sha256)
-		else if (!strcmp(argv[i], "-h")){
+		if (!strcmp(argv[i], "-h")){
 			if (i < argc - 2){
 				h_command = argv[i+1];
 				++i;
+				continue;
 			}
 			else {
 				printf("forensic [-r] [-h [md5[,sha1[,sha256]]] [-o <outfile>] [-v] <file|dir>\n");
-				return -1;
+				exit(1);
 			}
 		}
-		else if (!strcmp(argv[i], "-v")){
+		
+		if (!strcmp(argv[i], "-v")){
 			v_command = true;
+			continue;
 		}
-		else if (!strcmp(argv[i], "-o")){
+		
+		if (!strcmp(argv[i], "-o")){
 			if (i < argc - 2){
 				o_command = argv[i+1];
 				++i;
+				continue;
 			}
 			else {
 				printf("forensic [-r] [-h [md5[,sha1[,sha256]]] [-o <outfile>] [-v] <file|dir>\n");
-				return -1;
+				exit(1);
 			}
 		}
+		
 	}
 	
 	filepath = argv[argc-1];
-	
-	//DEBUG
-	printf("r: %d\n", r_command);
-	if (h_command == NULL)
-		printf("h: 0\n");
-	else printf("h: %s\n", h_command);
-	printf("v: %d\n", v_command);
-	if (o_command == NULL)
-		printf("o: 0\n");
-	else printf("o: %s\n", o_command);
-	printf("filepath: %s\n", filepath);
-	
-	return 0;	
+	analyze_file(filepath);//Testar um ficheiro para ja
 }
