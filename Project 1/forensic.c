fae1123e73455fc77f3a9f88026c66026226e646
nunomiguel22 <37627936+nunomiguel22@users.noreply.github.com>
2019-03-25 17:18:50 +0000

    include fix

--- Project 1/forensic.c
+++ Project 1/forensic.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdbool.h>
-#include <signal.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <time.h>
+#include <signal.h>
 
 void signal_handler(int signo) //handler do sinal
 {
