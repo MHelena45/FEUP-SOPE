2579d3397a548391950fb39aa2070244fa2cc94e
nunomiguel22 <37627936+nunomiguel22@users.noreply.github.com>
2019-03-26 17:56:55 +0000

    directory analysis

--- Project 1/forensic.c
+++ Project 1/forensic.c
@@ -2,21 +2,27 @@
 #include <string.h>
 #include <stdbool.h>
 #include <stdlib.h>
+#include <dirent.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
 #include <time.h>
 #include <signal.h>
 
-void signal_handler(int signo) //handler do sinal
+
+void sigint_handler(int signo) //handler do sinal
 {
+	/*Quando recebe o comando ctrl+c o programa e todos os processos filhos tem que terminar quando 
+	ja nao tiver operaçoes pendentes.
+	*/
+	
     sleep(10);
     exit(0);
 
-} 
-
-int analyze_path (char *filepath) {
-	/*
+}
+int analyze_file (char *filepath, struct stat *statdata, char *filename){
+	
+		/*
 		Comando stat(man 2 stat), da um struct com file_size(st_size), file_access(st_mode), 
 		file_modification_date(st_mtime), file_created(st_ctime, last status change?)	
 		Comando file da file_type(?)
@@ -28,8 +34,21 @@ int analyze_path (char *filepath) {
 			-printf da informaçao ou gravar em ficheiro quando o_commmand diferente de null
 	*/
 	
+		struct tm mt;
+		char *mod_time = (char*) malloc (19);
+		localtime_r(&statdata->st_mtime, &mt);
+		sprintf(mod_time, "%d-%d-%dT%d:%d:%d", 1900 + mt.tm_year, mt.tm_mon, mt.tm_mday, mt.tm_hour, mt.tm_min, mt.tm_sec);
+	
+		//DEBUG
+		printf("%s,file_type,%ld,file access (parse st_mode),%s\n", filename, statdata->st_size, mod_time);
+	
+		free(mod_time);
+		return 0;
+}
+
+int analyze_path (char *filepath) {
+
 	struct stat statdata;
-	struct tm mt;
 	
 	if (stat(filepath, &statdata) < 0){
 		perror("Error");
@@ -37,40 +56,49 @@ int analyze_path (char *filepath) {
 	}
 	
 	if (S_ISDIR(statdata.st_mode)){ //Directory
+		
 		printf("This is a directory, TODO\n");
-		exit(2);
+		
+		DIR *c_dir;
+		struct dirent *dir;
+		char temp_filename[257];
+		if ( (c_dir = opendir(filepath)) == NULL ){
+			perror("Error");
+			exit(1);
+		}
+		
+		while ( (dir = readdir(c_dir)) != NULL)  {
+			
+			struct stat temp_stat;
+			sprintf(temp_filename, "%s/%s", filepath, dir->d_name);
+			if (stat(temp_filename, &temp_stat) < 0){
+				perror("Error");
+				exit(1);	
+			}
+			if (S_ISREG(temp_stat.st_mode))
+				analyze_file(temp_filename, &temp_stat, dir->d_name);
+		}
 	}
 	else { //Not a directory, regular file, symbolic link, etc
-		char *mod_time = (char*) malloc (19);
-		localtime_r(&statdata.st_mtime, &mt);
-		sprintf(mod_time, "%d-%d-%dT%d:%d:%d", 1900 + mt.tm_year, mt.tm_mon, mt.tm_mday, mt.tm_hour, mt.tm_min, mt.tm_sec);
-	
-		//DEBUG
-		printf("file_name,file_type,%ld,file access (parse st_mode),%s\n", statdata.st_size, mod_time);
-	
-		free(mod_time);
+		analyze_file(filepath, &statdata, filepath);
 	}
-	
-	
-	
-	
-	
+		
 	return 0;
 }
 
 int main (int argc, char *argv[], char *envp[]){
-
+	
 	struct sigaction action;
-    action.sa_handler = signal_handler;
-    sigemptyset(&action.sa_mask);
+	action.sa_handler = sigint_handler;
+	sigemptyset(&action.sa_mask);
 
-    if (sigaction(SIGINT,&action,NULL) < 0) //tratamento de sinal associado ao ctrl+c
-    {
+	if (sigaction(SIGINT,&action,NULL) < 0) //tratamento de sinal associado ao ctrl+c
+	{
 		fprintf(stderr,"Unable to install SIGTERM handler\n");
 		exit(1);
-    }
+	}
 	
-	char* filepath;			//File or Dir path
+	char* filepath;	//File or Dir path
 	bool r_command = false;	//-r flag, analyse all files and subdirectories
 	char *h_command = NULL;	//-h flag, string with algorithms, null if not used
 	bool v_command = false;	//-v flag, generate log file, log file name in enviromnent variable LOGFILENAME
