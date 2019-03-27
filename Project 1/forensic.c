48f382eb21e1436b6b50155b314da6b3f24e036e
Helena Ferreira <44232337+MHelena45@users.noreply.github.com>
2019-03-27 11:25:26 +0000

    comentario ficam melhor no header file

--- Project 1/forensic.c
+++ Project 1/forensic.c
@@ -1,21 +1,5 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdbool.h>
-#include <stdlib.h>
-#include <dirent.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <unistd.h>
-#include <time.h>
-#include <signal.h>
-
-
 void sigint_handler(int signo) //handler do sinal
-{
-	/*Quando recebe o comando ctrl+c o programa e todos os processos filhos tem que terminar quando 
-	ja nao tiver operaçoes pendentes.
-	*/
-	
+{		
     sleep(10);
     exit(0);
 
